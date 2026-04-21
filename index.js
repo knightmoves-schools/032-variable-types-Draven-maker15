4853802299fbefe116cb1e96c0510a0b9e693472
Draven-maker15 <draven.davenport@students.msswarriors.org>
2026-04-21 13:48:16 -0500

    Fix variable usage in document element update

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ let animalName = "fido";
 let height = 24;
 let isIndoorPet = true;
 
-documant.getElementById("result").innerHTML = type of animalName;
+document.getElementById("result").innerHTML = animalName;
